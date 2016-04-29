6f363511057480065d1d50dafb50eb6d36dd5fcd
Anne Kao <annekao94@gmail.com>
2016-04-29 16:09:06 -0700

    logged into spotify! working on mysql next

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
 var express = require('express');
+var request = require('request');
 var bodyParser = require('body-parser');
 var app = express();
 
 app.use(bodyParser.json()); // for parsing application/json
 
-app.all('/', function(req, res, next) {
+app.all('*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
 });
 
-app.get('/', function(req, res) {
-  var body = req.body;
-  console.log(body);
-  res.json(req.body);
+app.get('/api/v1/spotify/me', function(req, res) {
+  var accessToken = req.query.access_token;
+
+  var options= {
+    url: 'https://api.spotify.com/v1/me',
+    headers: {
+      'Authorization': 'Bearer ' + accessToken
+    }
+  }
+  request(options, function(err, resp, body) {
+    body = JSON.parse(body);
+    console.log(body);
+    res.send(body);
+  });
+
 });
 
 app.listen(8000, function() {
--- client/app/routes/index.js
+++ client/app/routes/index.js
@@ -1,48 +1,42 @@
 import Ember from 'ember';
 
-function spotifyLogin(callback)
-{
-  var CLIENT_ID = '4352ff0df1ed44599107b6b1eea51bbc';
-  var REDIRECT_URI = 'http://localhost:4200';
-
-  function getLoginURL(scopes) {
-    return 'https://accounts.spotify.com/authorize?client_id=' + CLIENT_ID +
-      '&redirect_uri=' + encodeURIComponent(REDIRECT_URI) +
-      '&scope=' + encodeURIComponent(scopes.join(' ')) +
-      '&response_type=token';
+function getHashParams() {
+  var hashParams = {};
+  var e, r = /([^&;=]+)=?([^&;]*)/g,
+    q = window.location.hash.substring(1);
+  while ( e = r.exec(q)) {
+    hashParams[e[1]] = decodeURIComponent(e[2]);
   }
-
-  var url = getLoginURL([
-    'user-read-email',
-    'playlist-modify-public'
-  ]);
-
-  var width = 450,
-    height = 730,
-    left = (screen.width / 2) - (width / 2),
-    top = (screen.height / 2) - (height / 2);
-
-  window.addEventListener("message", function (event) {
-    var hash = JSON.parse(event.data);
-    if (hash.type == 'access_token') {
-      callback(hash.access_token);
-    }
-  }, false);
-
-  var w = window.open(url,
-    'Spotify',
-    'menubar=no,location=no,resizable=no,scrollbars=no,status=no, width=' + width + ', height=' + height + ', top=' + top + ', left=' + left
-  );
+  return hashParams;
 }
 
 export default Ember.Route.extend({
+  model() {
+    var params = getHashParams();
+    if (params.access_token !== undefined) {
+      return $.getJSON('http://localhost:8000/api/v1/spotify/me?access_token='+params.access_token).then(function(response){
+        
+      });
+    }
+  },
   actions: {
     login() {
-      spotifyLogin(function (accessToken) {
-        console.log(accessToken);
-        document.getElementById('login-container').style.display = 'none';
-        document.getElementById('home-container').innerHTML = "potato";
-      });
+      var CLIENT_ID = '4352ff0df1ed44599107b6b1eea51bbc';
+      var REDIRECT_URI = 'http://localhost:4200';
+
+      function getLoginURL(scopes) {
+        return 'https://accounts.spotify.com/authorize?client_id=' + CLIENT_ID +
+          '&redirect_uri=' + encodeURIComponent(REDIRECT_URI) +
+          '&scope=' + encodeURIComponent(scopes.join(' ')) +
+          '&response_type=token&show_dialog=true';
+      }
+
+      var url = getLoginURL([
+        'user-read-email',
+        'playlist-modify-public'
+      ]);
+
+      window.location.replace(url);
     }
   }
 
